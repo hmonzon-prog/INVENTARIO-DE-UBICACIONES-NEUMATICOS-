fca4d9fb50af2568d82a742f2752efb1fcbd999a
Hmonzon <hmonzon@prog.com>
2026-08-01 18:21:02 -0300

    fix: remove black calle line, keep gray pasillo

--- app.js
+++ app.js
@@ -517,7 +517,6 @@ function renderMapa() {
     odds.forEach(n => { h += renderDot(nave, letra, n, 'right'); });
     h += '</div>';
     h += '<div class="pasillo-line"></div>';
-    h += '<div class="calle-line"></div>';
     h += '<div class="pb-col">';
     evens.forEach(n => { h += renderDot(nave, letra, n, 'left'); });
     h += '</div>';
